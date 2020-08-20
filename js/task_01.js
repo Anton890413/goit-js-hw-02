bdb19bab6615dd9fac2bb6271303afa8d1491582
Anton890413 <antonzaycev1989@gmail.com>
2020-08-20 18:06:40 +0300

    two tasks were done

--- js/task_01.js
+++ js/task_01.js
@@ -1,8 +1,34 @@
-"use strict";
-const getItemsString = ["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"];
-const result = getItemsString.length - 1;
-for (let i = 0; i <= getItemsString.length - 1; i += 1) {
-    getItemsString[i] = ${ };
-  getItemsString[i] += `'${result}-\n`;
+/*"use strict";
+const elements = ["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"];
+let string = "";
+for (const element of elements) {
+  string += element + "\n";
 }
-console.log(getItemsString);
+string = string.slice(0, string.length - 1);
+console.log(string);*/
+
+/*const getItemsString = function (['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']) {
+  'use strict';
+  let getItemsString = '';
+  for (i = 0; i <= getItemsString.length - 1; i += 1) { };
+
+};
+
+console.log(getItemsString());*/
+/*
+'1 - Mango
+2 - Poly
+3 - Ajax
+4 - Lux
+5 - Jay
+6 - Kong
+'
+*/
+
+//console.log(getItemsString([5, 10, 15]));
+/*
+'1 - 5
+2 - 10
+3 - 15
+'
+*/
---
+++ js/task_02.js
@@ -0,0 +1,15 @@
+const calculateEngravingPrice = function (message, pricePerWord = 0) {
+  pricePerWord <= calculateEngravingPrice;
+  message = message.split([" "]);
+  message = message.length;
+  pricePerWord;
+  const result = message * pricePerWord;
+  if (message === "" && pricePerWord === undefined) {
+    result = 0;
+  }
+  return result;
+};
+
+const firstTask = calculateEngravingPrice("", undefined);
+
+console.log(firstTask);
